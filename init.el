7a7a19129462fd9c935cfa52665273797025fd6c
ManuelAngelSerrano <Manuel.Serrano@uclm.es>
2017-04-07 20:55:46 +0200

    Some cleaning done

--- init.el
+++ init.el
@@ -14,11 +14,6 @@
 (setq x-select-enable-clipboard t)
 (setq mac-pass-command-to-system t)
 
-;;(setq mac-pass-command-to-system t)
-;;(setq mac-option-modifier nil
-;;      mac-command-modifier 'meta
-;;      x-select-enable-clipboard t)
-
 ;; UTF-8 as default encoding
 (set-language-environment "UTF-8")
 
@@ -30,6 +25,9 @@
 
 ;; Word Wrap
 (global-visual-line-mode 1)
+
+;; linum-mode -> Line numbers
+(linum-mode 1)
 
 ;; markdown-mode
 ;;;;;;;;;;;;;;;;
@@ -47,7 +45,6 @@
 ;;pandoc-mode
 ;;;;;;;;;;;;;;
 (add-hook 'markdown-mode-hook 'pandoc-mode)
-;(add-hook 'pandoc-mode-hook (lambda () (setq pandoc-binary "/usr/local/bin/pandoc"))
 
 ;;ido-vertical-mode
 (require 'ido-vertical-mode)
@@ -60,50 +57,23 @@
 
 (global-set-key (kbd "C-<SPC>") 'set-mark-command) ; Ctrl-SPC Set Mark
 
-(global-set-key (kbd "C-k") 'kill-whole-line) ; Ctrl-K Kill
+;; (global-set-key (kbd "C-k") 'kill-whole-line) ; Ctrl-K Kill
 
-(global-set-key (kbd "C-<tab>") 'other-window) ; Ctrl-TAB Next Window Buffer
-
-;(global-set-key (kbd  "M-4") 'split-window-horizontally) ; Alt-4 abrir ventana a la derecha
-;(global-set-key (kbd  "M-S-4") 'split-window-vertically) ; M-Shift-4 abrir ventana abajo
+;; (global-set-key (kbd "C-<tab>") 'other-window) ; Ctrl-TAB Next Window Buffer
 
 (global-set-key (kbd "C-q") 'save-buffers-kill-emacs) ; Ctrl-Q Quit
 
 (define-key isearch-mode-map (kbd "<right>") 'isearch-repeat-forward) ; -> Search Forward
 (define-key isearch-mode-map (kbd "<left>") 'isearch-repeat-backward) ; <- Search Backward
 
-;; (global-set-key (kbd "M-<left>") 'backward-word)
-;; (global-set-key (kbd "M-<right>") 'forward-word)
-;; (global-set-key (kbd "M-<up>") 'backward-paragraph)
-;; (global-set-key (kbd "M-<down>") 'forward-paragraph)
-;; (global-set-key (kbd "M-p") 'backward-paragraph)
-;; (global-set-key (kbd "M-n") 'forward-paragraph)
-
 (global-set-key (kbd "M-x") 'smex)
-
 
 (global-set-key (kbd "C-z") 'undo) ; Ctrl-Z Undo
 (global-set-key (kbd "C-S-Z") 'redo) ; Ctrl-Shift-Z Redo
 
-(global-set-key (kbd "C-'") 'comment-dwim) ; Ctrl-ç Comment
+;; (global-set-key (kbd "C-'") 'comment-dwim) ; Ctrl-ç Comment
 
 (define-key text-mode-map (kbd "<tab>") 'tab-to-tab-stop)
-;(define-key markdown-mode-map (kbd "C-b") 'pandoc-convert-to-pdf)
-
-;;Reescribimos las teclas del markdown-mode
-;; (defun mp-add-markdown-keys ()
-;;   (local-set-key (kbd "M-<left>") 'backward-word)
-;;   (local-set-key (kbd "M-<right>") 'forward-word)
-;;   (local-set-key (kbd "M-b") 'backward-word)
-;;   (local-set-key (kbd "M-f") 'forward-word)
-;;   (local-set-key (kbd "M-p") 'backward-paragraph)
-;;   (local-set-key (kbd "M-n") 'forward-paragraph)
-;;   (local-set-key (kbd "M-<up>") 'backward-paragraph)
-;;   (local-set-key (kbd "M-<down>") 'forward-paragraph)
-;; )
-
-;; (add-hook 'markdown-mode-hook 'mp-add-markdown-keys)
-;;Fin Keybind markdown-mode
 
 ;; redo+
 (require 'redo+)
@@ -135,34 +105,6 @@
 (package-initialize)
 (smartparens-global-mode t)
 (require 'smartparens-config)
-
-
-;; ;; Autocomplete
-;; (add-hook 'c-mode-common-hook '(lambda ()
-;;   ;; ac-omni-completion-sources is made buffer local so
-;;   ;; you need to add it to a mode hook to activate on
-;;   ;; whatever buffer you want to use it with.  This
-;;   ;; example uses C mode (as you probably surmised).
-
-;;   ;; auto-complete.el expects ac-omni-completion-sources to be
-;;   ;; a list of cons cells where each cell's car is a regex
-;;   ;; that describes the syntactical bits you want AutoComplete
-;;   ;; to be aware of. The cdr of each cell is the source that will
-;;   ;; supply the completion data.  The following tells autocomplete
-;;   ;; to begin completion when you type in a . or a ->
-
-;;   (add-to-list 'ac-omni-completion-sources
-;;                (cons "\\." '(ac-source-semantic)))
-;;   (add-to-list 'ac-omni-completion-sources
-;;                (cons "->" '(ac-source-semantic)))
-
-;;   ;; ac-sources was also made buffer local in new versions of
-;;   ;; autocomplete.  In my case, I want AutoComplete to use
-;;   ;; semantic and yasnippet (order matters, if reversed snippets
-;;   ;; will appear before semantic tag completions).
-
-;;   (setq ac-sources '(ac-source-semantic ac-source-yasnippet))
-;; ))
 
 ;; haml-mode
 (require 'haml-mode)
@@ -223,11 +165,6 @@
 ;;Para el auto completado
 (add-hook 'after-init-hook 'global-company-mode)
 
-
-;(setq pandoc-binary "/usr/local/bin/pandoc")
-
-(linum-mode 1)
-
 (require 'evil-leader)
 (global-evil-leader-mode)
 (evil-leader/set-leader ",")
@@ -252,8 +189,6 @@
 (define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
-
-;(define-key evil-insert-state-map (kbd "C-j") 'evil-normal-state)
 
 ;;neotree
 (require 'neotree)
